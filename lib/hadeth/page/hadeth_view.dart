6d9db3b93205e1d7c9a18e37f7ef591379d98f09
No Respect <norespect326@gmail.com>
2024-02-15 03:30:16 +0200

    finish hadeth feature

--- lib/hadeth/page/hadeth_view.dart
+++ lib/hadeth/page/hadeth_view.dart
@@ -1,19 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islam_app_c10_sun_4pm/hadeth/page/hadeth_details.dart';
 
-class HadethView extends StatelessWidget {
+class HadethView extends StatefulWidget {
   const HadethView({super.key});
 
   @override
+  State<HadethView> createState() => _HadethViewState();
+}
+
+class _HadethViewState extends State<HadethView> {
+  List<HadethData> allHadethContent = [];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Hadeth View",
-        style: TextStyle(
-          fontSize: 50,
-          fontWeight: FontWeight.bold,
-          fontFamily: "El Messeri",
+    var mediaQuery = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+    // call function of load data
+    //هروح انده علي load dataلو حدسث فاضي
+
+    if (allHadethContent.isEmpty) loadData();
+    return Column(
+      children: [
+        SizedBox(
+          height: mediaQuery.height * 0.05,
         ),
-      ),
+        Image.asset(
+          "assets/images/hadeth_header.png",
+          height: mediaQuery.height * 0.2,
+        ),
+        Divider(
+          thickness: 1.2,
+          color: theme.primaryColor,
+        ),
+        const Text(
+          "الأحاديث",
+          style: TextStyle(
+            fontSize: 25,
+            fontFamily: "El Messiri",
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Divider(
+          thickness: 1.2,
+          color: theme.primaryColor,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  HadethDetailsView.routeName,
+                  arguments: allHadethContent[index],
+                );
+              },
+              child: Text(
+                allHadethContent[index].title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontFamily: "El Messiri",
+                  fontSize: 22,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            itemCount: allHadethContent.length,
+          ),
+        ),
+      ],
     );
   }
+
+// create function of loadData
+  Future<void> loadData() async {
+    String Content = await rootBundle.loadString("assets/files/ahadeth.txt");
+    List<String> allHadethList = Content.split("#");
+    for (int i = 0; i < allHadethList.length; i++) {
+      String singleHadeth = allHadethList[i].trim();
+      int titleIndex = singleHadeth.indexOf("\n");
+      String title = singleHadeth.substring(0, titleIndex);
+      String content = singleHadeth.substring(titleIndex + 1);
+      HadethData hadethData = HadethData(title, content);
+      allHadethContent.add(hadethData);
+      setState(() {});
+    }
+  }
+}
+
+class HadethData {
+  final String title;
+  final String content;
+
+  HadethData(this.title, this.content);
 }
